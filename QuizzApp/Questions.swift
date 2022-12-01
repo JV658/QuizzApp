4100d1145ae50dea52d2bb21e5d4c34397ad177a
Joshua Vand der Most <joshua.vandermost@cambriancollege.ca>
2022-12-01 18:41:41 -0500

    animations!!!

---
+++ QuizzApp/Questions.swift
@@ -0,0 +1,18 @@
+//
+//  Questions.swift
+//  QuizzApp
+//
+//  Created by Cambrian on 2022-12-01.
+//
+
+import Foundation
+
+struct Questions{
+    var qa: [String: String] = [:]
+    
+    init(){
+        qa["What is 2 + 2"] = "4"
+        qa["What does API stand for"] = "Application Program Interface"
+        qa["what is the latest iPhone"] = "iPhone 14"
+    }
+}
--- QuizzApp/ViewController.swift
+++ QuizzApp/ViewController.swift
@@ -8,12 +8,61 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var questions = Questions()
+    var qs: [String] = []
+    
+    @IBOutlet weak var question1Constraint: NSLayoutConstraint!
+    @IBOutlet weak var question2Constraint: NSLayoutConstraint!
+    @IBOutlet weak var question2: UILabel!
+    @IBOutlet weak var question: UILabel!
+    @IBOutlet weak var answer: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        qs = questions.qa.keys.sorted(by: <)
+        
+        let q = qs[Int.random(in: 0..<qs.count)]
+        
+        let a = questions.qa[q]
+
+        answer.alpha = 0
+        
+        question.text = q
+        answer.text = a
+        
+        let screenWidth = view.frame.width
+        question2Constraint.constant = -screenWidth
+        
     }
+    @IBAction func showAnswer(_ sender: Any) {
 
-
+        
+        UIView.animateKeyframes(withDuration: 3, delay: 0) {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1) {
+                self.answer.alpha = 1
+            }
+        }
+    }
+    
+    @IBAction func nextQuestion(_ sender: Any) {
+        let q = qs[Int.random(in: 0..<qs.count)]
+        
+        let a = questions.qa[q]
+        
+        answer.alpha = 0
+        
+        question.text = q
+        answer.text = a
+        
+        UIView.animate(withDuration: 2) {
+            self.question2Constraint.constant = 0
+            self.view.layoutIfNeeded()
+        }
+        
+        
+        
+    }
+    
 }
 
